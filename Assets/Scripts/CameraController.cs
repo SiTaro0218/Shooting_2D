fe4a29a8a35135379dde32ed17c9649e109d1b32
Ryotaro Shiro <4623051@ed.tus.ac.jp>
2025-11-24 07:15:17 +0900

    Update: マップを生成，ランダムで詳細なタイルを設置

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // スクロール速度 (Inspectorから調整できるようにpublicにする)
+    public float scrollSpeed = 2.0f;
+
+    // ゲームが進行中かどうかのフラグ
+    private bool isScrolling = true;
+
+    // Start is called before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 初期状態として、カメラはスクロールを開始する
+        isScrolling = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isScrolling)
+        {
+            // 現在のカメラの位置を取得
+            Vector3 currentPosition = transform.position;
+
+            // Y軸方向に scrollSpeed * 1秒間の経過時間 分だけ移動
+            // Time.deltaTimeを使うことで、フレームレートに関わらず一定速度で移動できる
+            currentPosition.y += scrollSpeed * Time.deltaTime;
+
+            // カメラの位置を更新
+            transform.position = currentPosition;
+        }
+    }
+
+    // スクロールを停止・再開するためのメソッド（GameManagerから呼び出す想定）
+    public void SetScrolling(bool scrolling)
+    {
+        isScrolling = scrolling;
+    }
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class MapGenerator : MonoBehaviour
+{
+    // Layer 1: Ground
+    public Tilemap backgroundTilemap;
+    public TileBase groundTile;
+
+    // Layer 2: Details
+    public Tilemap detailTileMap;
+    public TileBase waterTile;
+    public TileBase mountainTile;
+
+    public int mapWidth = 10;
+    public int mapHeight = 30;
+    public float Probability = 0.1f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    void GenerateMap()
+    {
+        for (int x = -mapWidth / 2; x < mapWidth / 2; x++)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                Vector3Int pos = new Vector3Int(x, y, 0);
+                // 1. 背景タイルを配置
+                backgroundTilemap.SetTile(pos, groundTile);
+
+                // 2. 詳細タイルをランダムに配置
+                if (Random.Range(0f, 1f) < Probability) // 確率で水タイルを配置
+                {
+                    detailTileMap.SetTile(pos, waterTile);
+                }
+                else if (Random.Range(0f, 1f) < Probability) // 確率で山タイルを配置
+                {
+                    detailTileMap.SetTile(pos, mountainTile);
+                }
+            }
+        }
+    }
+}
